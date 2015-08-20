a3fff4fcfbe7daf107300073363b37a19ac771a2
nero <louis.vanbiervliet@gmail.com>
2015-08-20 09:22:40 +0200

    index toont enkel votes van de vorige ronde meerdere keren voten op zelfde spel geeft nu error message

---
+++ reset.php
@@ -0,0 +1,12 @@
+<?php
+
+$whovoted = array();
+
+$serializedData = serialize($whovoted); 
+file_put_contents('1/whovoted.data', $serializedData);
+
+$votes = serialize(array());
+file_put_contents('1/votesPrevRound.data', $votes);
+
+file_put_contents('1/votes.data', $votes);
+?>
